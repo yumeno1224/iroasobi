2423bfe19e763e107cabc9bde2953ff64d71a144
Haru1028 <fiel.vill2818@gmail.com>
2025-06-17 10:21:36 +0900

    fix: deploy correct web build to docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0a8a3a72180ea584d10c2ef7b59c6d53
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a217e0079e914ba6c67d7c21523a4bfb",
+"flutter_bootstrap.js": "071d1f705a06baf68b319fbaccb6f28e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
