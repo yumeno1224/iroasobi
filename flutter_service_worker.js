a3bebbb3dfd3c89aa37897cf1245199c913b3ea4
Haru1028 <fiel.vill2818@gmail.com>
2025-06-23 19:13:41 +0900

    Fix deploy structure

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0a8a3a72180ea584d10c2ef7b59c6d53
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f2ecd74569f949fdcdc945682c820db2",
+"flutter_bootstrap.js": "d5e2ae0472f0e2b0ccc59a89e65e6fa4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
