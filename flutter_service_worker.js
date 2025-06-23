d802801437d6edd920e39c40f340d2f4524d5fa4
Haru1028 <fiel.vill2818@gmail.com>
2025-06-23 18:23:32 +0900

    Deploy latest web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0a8a3a72180ea584d10c2ef7b59c6d53
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1df210ce1969ee881142890979a19e93",
+"flutter_bootstrap.js": "1fda9295a0aa1fb74aa60464a8c7bfcf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
